4e2f7b7b7cdfdb51674b3045cbfa8c3923ff9411
norman-ma <mikexcelsior@gmail.com>
2018-09-03 12:41:35 -0500

    migration issues

--- app/models.py
+++ app/models.py
@@ -13,9 +13,6 @@ class Card(db.Model):
     spells = db.relationship('SummonerSpell', backref='card', lazy=True, uselist=False)
     items = db.relationship('Item', backref='card', lazy=True, uselist=False)
 
-    def get_id(self):
-        return str(self.id)  # python 3 support
-
 
 class Champion(Card):
     __tablename__ = 'champion'
@@ -32,12 +29,6 @@ class Champion(Card):
     type2 = db.Column(db.String(12))
     pets = db.relationship('Pet', backref='champion', lazy=True)
 
-    def get_id(self):
-        return str(self.id)  # python 3 support
-
-    def __repr__(self):
-        return '<Champion %r>' % (self.name)
-
 
 class Pet(Card):
     __tablename__ = 'pet'
@@ -46,12 +37,6 @@ class Pet(Card):
     hp = db.Column(db.Integer)
     ad = db.Column(db.Integer)
     belongs_to = db.Column(db.Integer, db.ForeignKey('champion.card_id'))
-
-    def get_id(self):
-        return str(self.id)  # python 3 support
-
-    def __repr__(self):
-        return '<Pet %r>' % (self.name)
 
 
 class MeutralMonster(Card):
@@ -62,24 +47,12 @@ class MeutralMonster(Card):
     ad = db.Column(db.Integer)
     monster_type = db.Column(db.String(12))
 
-    def get_id(self):
-        return str(self.id)  # python 3 support
-
-    def __repr__(self):
-        return '<NeutralMonster %r>' % (self.name)
-
 
 class SummonerSpell(Card):
     __tablename__ = 'summoner_spell'
     __table_args__ = {'extend_existing': True}
     card_id = db.Column(db.Integer, db.ForeignKey('card.card_id'), primary_key=True, nullable=False)
     spell_type = db.Column(db.String(14))
-
-    def get_id(self):
-        return str(self.id)  # python 3 support
-
-    def __repr__(self):
-        return '<SummonerSpell %r>' % (self.name)
 
 
 class Item(Card):
@@ -91,21 +64,9 @@ class Item(Card):
     ap = db.Column(db.Integer)
     has = db.relationship('ItemHas', backref='item', lazy=True)
 
-    def get_id(self):
-        return str(self.id)  # python 3 support
-
-    def __repr__(self):
-        return '<Item %r>' % (self.name)
-
 
 class ItemHas(db.Model):
     __tablename__ = 'item_has'
     card_id = db.Column(db.Integer, db.ForeignKey('item.card_id'), primary_key=True, nullable=False)
     stat_name = db.Column(db.String(20))
     qty = db.Column(db.Integer)
-
-    def get_id(self):
-        return str(self.id)  # python 3 support
-
-    def __repr__(self):
-        return '<ItemHas %r>' % (self.name)
