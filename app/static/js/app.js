fb20c498f2c43df7227f194529c6621a55cec554
norman-ma <mikexcelsior@gmail.com>
2018-10-19 16:54:37 -0500

    v2.5

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -158,9 +158,8 @@ app.controller("DeckController",["$scope","$http","$rootScope",function($scope,$
     };
 
     $scope.sort = function(){
-        console.log($scope.deck.cards);
         $scope.deck.cards = qsort($scope.deck.cards);
-        console.log($scope.deck.cards);
+        distribute();
     };
 
     var distribute = function(){
